4726777d898b0876a2df27012784469fe8c04b3b
ArtemTeleluhin <teleluhin.artem@yandex.ru>
2021-04-14 20:29:18 +0300

    Добавлен обработчик menu, возвращающий меню в формате json

--- server/main.py
+++ server/main.py
@@ -1,150 +1,40 @@
-from flask import Flask
+from flask import Flask, jsonify
 from server.data import db_session
 from server.data.categories import Categories
 from server.data.dishes import Dishes
 from server.data.versions import Versions
-
-MENU = {
-    'Пиццы': [
-        {
-            'name': 'Маргарита',
-            'proportions': [
-                {
-                    'size': '25 см',
-                    'price': 150
-                },
-                {
-                    'size': '35 см',
-                    'price': 200
-                },
-                {
-                    'size': '45 см',
-                    'price': 250
-                }
-            ],
-            'add_info': 'Помидоры, сыр Моцарелла, базилик, томатный соус'
-        },
-        {
-            'name': 'Пепперони',
-            'proportions': [
-                {
-                    'size': '25 см',
-                    'price': 150
-                },
-                {
-                    'size': '35 см',
-                    'price': 200
-                },
-                {
-                    'size': '45 см',
-                    'price': 250
-                }
-            ],
-            'add_info': 'Колбаса, томатный соус, сыр'
-        },
-        {
-            'name': 'Мясная',
-            'proportions': [
-                {
-                    'size': '25 см',
-                    'price': 150
-                },
-                {
-                    'size': '35 см',
-                    'price': 200
-                },
-                {
-                    'size': '45 см',
-                    'price': 250
-                }
-            ],
-            'add_info': 'Ветчина, колбаса, томатный соус, сыр'
-        }
-    ],
-    'Напитки': [
-        {
-            'name': 'Кока-Кола',
-            'proportions': [
-                {
-                    'size': '0.3 л',
-                    'price': 45
-                },
-                {
-                    'size': '0.7 л',
-                    'price': 90
-                },
-                {
-                    'size': '1 л',
-                    'price': 110
-                }
-            ]
-        },
-        {
-            'name': 'Яблочный сок',
-            'proportions': [
-                {
-                    'size': '0.3 л',
-                    'price': 40
-                },
-                {
-                    'size': '0.7 л',
-                    'price': 80
-                },
-                {
-                    'size': '1 л',
-                    'price': 100
-                }
-            ]
-        }
-    ],
-    'Десерты': [
-        {
-            'name': 'Маффин',
-            'proportions': [
-                {
-                    'size': 'Обычный',
-                    'price': 50
-                }
-            ]
-        },
-        {
-            'name': 'Круассан с шоколадом',
-            'proportions': [
-                {
-                    'size': 'Обычный',
-                    'price': 60
-                }
-            ]
-        }
-    ]
-}
 
 DB_NAME = 'pizzeria_base'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
+
+@app.route('/menu')
+def menu():
+    db_sess = db_session.create_session()
+    menu = dict()
+    for category in db_sess.query(Categories).all():
+        menu[category.name] = []
+        for dish in category.dishes:
+            if dish.is_sale:
+                dish_dict = dict()
+                dish_dict['name'] = dish.name
+                if dish.add_info:
+                    dish_dict['add_info'] = dish.add_info
+                dish_dict['proportions'] = []
+                for version in dish.versions:
+                    dish_dict['proportions'].append({
+                        'size': version.size,
+                        'price': version.price
+                    })
+                if dish_dict['proportions']:
+                    menu[category.name].append(dish_dict)
+        if not menu[category.name]:
+            menu.pop(category.name)
+    return jsonify(menu)
+
+
 if __name__ == '__main__':
     db_session.global_init(f'db/{DB_NAME}.db')
-    db_sess = db_session.create_session()
-
-    for category_name in MENU:
-        category = Categories()
-        category.name = category_name
-        db_sess.add(category)
-        db_sess.commit()
-        for dish_obj in MENU[category_name]:
-            dish = Dishes()
-            dish.name = dish_obj['name']
-            if 'add_info' in dish_obj:
-                dish.add_info = dish_obj['add_info']
-            dish.category = category
-            db_sess.add(dish)
-            db_sess.commit()
-            for version_obj in dish_obj['proportions']:
-                version = Versions()
-                version.size = version_obj['size']
-                version.price = version_obj['price']
-                version.dish = dish
-                db_sess.add(version)
-                db_sess.commit()
+    app.run(port=8080, host='127.0.0.1', debug=True)

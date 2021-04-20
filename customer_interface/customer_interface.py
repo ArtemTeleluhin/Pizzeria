f9d4bdac5d940cfccc3f4f7fb816266107a9f1de
srzotova <sophyzotova@yandex.ru>
2021-04-20 16:10:58 +0300

    Добвалены выбор блюд из меню, оформлен интерфейс, добавлен интерфейс для заказа

---
+++ customer_interface/customer_interface.py
@@ -0,0 +1,180 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QLabel, QSpinBox
+
+import requests
+from flask import Flask, jsonify, request
+from pprint import pprint
+
+PIZZERIA_ADDRESS = "http://127.0.0.1:8080"
+NUMBER_OF_CURRENT_NOTES = 6
+
+
+class Product:
+    def __init__(self, type_of_product, name, proportions, add_info=''):
+        self.type_of_product = type_of_product
+        self.name = name
+        self.proportions = proportions
+        self.add_info = add_info
+        self.number_of_proportion = [0] * len(proportions)
+
+    def set_proportion(self, proportion, val):
+        self.number_of_proportion[proportion] = val
+
+    def get_name(self):
+        return self.name
+
+    def get_add_info(self):
+        return self.add_info
+
+    def get_type(self):
+        return self.type_of_product
+
+    def get_proportion(self):
+        return self.proportions
+
+    def take_number_of_proportion(self, ind):
+        return self.number_of_proportion[ind]
+
+
+class CollectOrder(QMainWindow):
+    def __init__(self, menu):
+        super().__init__()
+        self.counts = []
+        self.counts_to_proportion = {}
+        self.labels = []
+        self.current = 0
+        self.menu = menu
+        self.start('Всё')
+
+    def start(self, text, new_type=True):
+        if new_type:
+            self.current = 0
+        uic.loadUi('untitled.ui', self)
+        unique_types = set()
+        self.comboBox.addItem(text)
+        unique_types.add(text)
+        unique_types.add('Всё')
+        for type_of_product in self.menu.keys():
+            for product in self.menu[type_of_product]:
+                unique_types.add(product.get_type())
+        for unique_type in unique_types:
+            if unique_type == text:
+                continue
+            self.comboBox.addItem(unique_type)
+        self.labels.clear()
+        self.counts.clear()
+        self.initUI()
+        self.comboBox.currentTextChanged.connect(self.start)
+
+    def new_lbl(self, name):
+        self.labels.append(QLabel(self))
+        self.labels[-1].setText(name)
+
+    def new_val(self, val):
+        # print(val)
+        self.counts_to_proportion[self.sender()][0].set_proportion(self.counts_to_proportion[self.sender()][1], val)
+
+    def new_cnt(self, product, j):
+        self.counts.append(QSpinBox(self))
+        self.counts_to_proportion[self.counts[-1]] = [product, j]
+        self.counts[-1].setValue(product.take_number_of_proportion(j))
+        self.counts[-1].valueChanged.connect(self.new_val)
+
+    def open_basket(self):
+        self.second_form = Basket(self.menu)
+        self.second_form.show()
+
+    def forward_notes(self):
+        if self.current + NUMBER_OF_CURRENT_NOTES <= len(self.menu[self.comboBox.currentText()]):
+            self.current += NUMBER_OF_CURRENT_NOTES
+            self.start(self.comboBox.currentText(), False)
+
+    def back_notes(self):
+        if self.current - NUMBER_OF_CURRENT_NOTES >= 0:
+            self.current -= NUMBER_OF_CURRENT_NOTES
+            self.start(self.comboBox.currentText(), False)
+
+    def initUI(self):
+        self.setWindowTitle('Главная форма')
+        self.pushButton.clicked.connect(self.open_basket)
+        self.forward.clicked.connect(self.forward_notes)
+        self.back.clicked.connect(self.back_notes)
+        for i, product in enumerate(
+                self.menu[self.comboBox.currentText()][
+                self.current:min(self.current + NUMBER_OF_CURRENT_NOTES, len(self.menu[self.comboBox.currentText()]))]):
+            self.new_lbl(product.get_name())
+            self.gridLayout_3.addWidget(self.labels[-1], i, 0)
+            for j, proportion in enumerate(product.get_proportion()):
+                self.new_lbl(f"{proportion['size']}\n{str(proportion['price'])}руб.")
+                self.gridLayout_3.addWidget(self.labels[-1], i, 2 * j + 2)
+                self.new_cnt(product, j)
+                self.gridLayout_3.addWidget(self.counts[-1], i, 2 * j + 1)
+
+
+class Basket(QMainWindow):
+    def __init__(self, menu):
+        super().__init__()
+        uic.loadUi('basket.ui', self)
+        self.labels = []
+        self.menu = menu
+        self.last_row = 0
+        self.cost = 0
+        self.show_chosen_menu()
+
+    def new_lbl(self, name):
+        self.labels.append(QLabel(self))
+        self.labels[-1].setText(name)
+        # self.labels[-1].move(150, 150)
+
+    def show_row(self, product, proportion, id_proportion):
+        self.new_lbl(product.get_name())
+        self.gridLayout.addWidget(self.labels[-1], self.last_row, 0)
+        # print(product.take_number_of_proportion(id_proportion))
+        self.new_lbl(f" * {str(product.take_number_of_proportion(id_proportion))}")
+        self.gridLayout.addWidget(self.labels[-1], self.last_row, 2)
+        self.new_lbl(f"{proportion['size']}\n{str(proportion['price'])}руб.")
+        self.gridLayout.addWidget(self.labels[-1], self.last_row, 1)
+        self.last_row += 1
+
+    def show_chosen_menu(self):
+        for type_of_product in self.menu.keys():
+            for i, product in enumerate(self.menu[type_of_product]):
+                for j, proportion in enumerate(product.get_proportion()):
+                    # print(product.get_name(), product.take_number_of_proportion(j))
+                    if product.take_number_of_proportion(j) != 0:
+                        self.cost += product.take_number_of_proportion(j) * proportion['price']
+                        self.show_row(product, proportion, j)
+        self.lcdNumber.display(self.cost)
+
+
+def get_menu():
+    menu = {'Всё': []}
+    response = requests.get(f'{PIZZERIA_ADDRESS}/menu')
+    json_response = response.json()
+    for type_of_product in json_response.keys():
+        if type_of_product not in menu.keys():
+            menu[type_of_product] = []
+        for product in json_response[type_of_product]:
+            if 'add_info' in product.keys():
+                menu[type_of_product].append(Product(type_of_product,
+                                                     product['name'], product['proportions'], product['add_info']))
+                menu['Всё'].append(Product(type_of_product,
+                                           product['name'], product['proportions'], product['add_info']))
+            else:
+                menu[type_of_product].append(Product(type_of_product,
+                                                     product['name'], product['proportions']))
+                menu['Всё'].append(Product(type_of_product,
+                                           product['name'], product['proportions']))
+    return menu
+
+
+if __name__ == '__main__':
+    appF = QApplication(sys.argv)
+    # app.run(port=8080, host='127.0.0.1', debug=True)
+    ex = CollectOrder(get_menu())
+    # print(get_menu())
+    ex.show()
+    sys.exit(appF.exec())
